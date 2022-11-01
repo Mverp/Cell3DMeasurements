c06885416dc9e4c784bfb17e6155394436e11520
Mverp <Merijn.vanErp@radboudumc.nl>
2022-11-01 11:39:03 +0100

    Remove MCIB3D measurements after plugin update

--- src/main/java/configuration/Measurement_Selector.java
+++ src/main/java/configuration/Measurement_Selector.java
@@ -8,8 +8,8 @@ import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
 
 /**
- * Allow the user to set which of the measurements that are available in the Feature Extractor are going to be active. The choices are being stored as preferences via the Prefs class and can be retrieved as such. This class also provides the methods
- * to do this.
+ * Allow the user to set which of the measurements that are available in the Feature Extractor are going to be active. The choices are being stored as preferences via the Prefs class and can be
+ * retrieved as such. This class also provides the methods to do this.
  *
  * @author Merijn van Erp
  *
@@ -23,8 +23,7 @@ public class Measurement_Selector implements PlugIn
 	/**
 	 * Based on a measurement name, retrieve the preference setting.
 	 *
-	 * @param aMeasurementName
-	 *            The name of the measurement
+	 * @param aMeasurementName The name of the measurement
 	 *
 	 * @return True if the measurement should be calculated and be part of the Feature Extractor results, false otherwise
 	 */
@@ -37,8 +36,7 @@ public class Measurement_Selector implements PlugIn
 	/**
 	 * Get the preference name under which the measurement choice has been saved.
 	 *
-	 * @param aMeasurementName
-	 *            The name of the measurement
+	 * @param aMeasurementName The name of the measurement
 	 * @return The name with which the measurement choice should be stored in the Prefs
 	 */
 	private static String getPreferenceLabel(final String aMeasurementName)
@@ -50,10 +48,8 @@ public class Measurement_Selector implements PlugIn
 	/**
 	 * Based on a measurement name, set the choice preference setting.
 	 *
-	 * @param aMeasurementName
-	 *            The name of the measurement
-	 * @param aChoice
-	 *            The boolean choice to set
+	 * @param aMeasurementName The name of the measurement
+	 * @param aChoice          The boolean choice to set
 	 */
 	public static void setMeasurementPreference(final String aMeasurementName, final boolean aChoice)
 	{
@@ -66,10 +62,8 @@ public class Measurement_Selector implements PlugIn
 	 *
 	 * @see getGroupMeasurements
 	 *
-	 * @param aNames
-	 *            The list of measurement names to add. The order is very important and should coincide with the retrieval order after the dialog is closed!
-	 * @param aDialog
-	 *            The dialog to which the measurements check boxes are added
+	 * @param aNames  The list of measurement names to add. The order is very important and should coincide with the retrieval order after the dialog is closed!
+	 * @param aDialog The dialog to which the measurements check boxes are added
 	 */
 	private void addCheckboxGroup(final String[] aNames, final GenericDialog aDialog)
 	{
@@ -91,10 +85,8 @@ public class Measurement_Selector implements PlugIn
 	 *
 	 * @see addCheckboxGroup
 	 *
-	 * @param aMeasurementNames
-	 *            The list of names of the next measurement choices. The order is very important and should coincide with the order in which the names were added to the dialog!
-	 * @param aDialog
-	 *            The dialog to retrieve the choices from
+	 * @param aMeasurementNames The list of names of the next measurement choices. The order is very important and should coincide with the order in which the names were added to the dialog!
+	 * @param aDialog           The dialog to retrieve the choices from
 	 */
 	private void getGroupMeasurements(final String[] aMeasurementNames, final GenericDialog aDialog)
 	{
@@ -121,8 +113,8 @@ public class Measurement_Selector implements PlugIn
 		dialog.addMessage(SegmentMeasurements.MORPHOLIBJ_MEASUREMENTS, font);
 		addCheckboxGroup(SegmentMeasurements.MORPHOLIBJ_GROUP, dialog);
 
-		dialog.addMessage(SegmentMeasurements.MCIB3D_MEASUREMENTS, font);
-		addCheckboxGroup(SegmentMeasurements.MCIB3D_GROUP, dialog);
+//		dialog.addMessage(SegmentMeasurements.MCIB3D_MEASUREMENTS, font);
+//		addCheckboxGroup(SegmentMeasurements.MCIB3D_GROUP, dialog);
 
 		dialog.showDialog();
 
@@ -131,7 +123,7 @@ public class Measurement_Selector implements PlugIn
 
 			getGroupMeasurements(SegmentMeasurements.STANDARD_GROUP_NUCLEUS, dialog);
 			getGroupMeasurements(SegmentMeasurements.MORPHOLIBJ_GROUP, dialog);
-			getGroupMeasurements(SegmentMeasurements.MCIB3D_GROUP, dialog);
+//			getGroupMeasurements(SegmentMeasurements.MCIB3D_GROUP, dialog);
 
 			Prefs.savePreferences();
 		}
